92544d1f9b57826c11c7d22beb09056eebb53520
DjentMachine <44790709+DjentMachine@users.noreply.github.com>
2020-02-25 21:13:12 +0000

    Latest version: updated IR and ER

--- classes.py
+++ classes.py
@@ -6,238 +6,283 @@
 ##
 
 ##
-# TODO: Take care of variable AMax. It's not set to 1000, as its the max possible value
+# TODO: Think about AMin possibility. Does it make sense for a user to set min and max values for area?
+# TODO: understand why both math and sqrt are needed
+# TODO: updat timeRichness in islands and archipelagos. Make one dependent of the other (IMPORTANT)
+# TODO: set __str__ instead of creating a toString as in java
+# TODO: Pass Update into Island class
 ##
 
 import random
-import numpy as np
-from math import sqrt
+import statistics as st
+from haversine import *
+# import numpy as np
+import pandas as pd
+from scipy import stats
 import math
 
-class island:
-    """
-    Constructor for the island class:
-        popSize -
-        initRichness -
-        isML -
-        area - area of an island
-        richness - species list for a given island
-    """
 
-    def __init__(self, sppCapacity, isML=False, coordRange=[38, -26], aWidth=2, initRichness = 1):
+class Island:
+    def __init__(self, sppCapacity, isML=False, coordRange=[[32, 40], [-24,-32]], aMax=10000, initRichness = 1):
+        """
+        Constructor for the island class:
+        sppCapacity: species capacity for the island
+        isML: Is the island a main land?
+        coordRange: ficticious WGS coordinates for the archipelago.
+        Amax: maximum area value possible
+        initRichness: initial number of species in the island
+        """
         self.isML = isML
-        self.area = random.randint(50, 1000)
-        self.coords = coordRange
-            #[random.uniform(coordRange[0] - aWidth, coordRange[0] + aWidth),
-             #          random.uniform(coordRange[1] - aWidth, coordRange[1] + aWidth)]
-        if isML: self.richness = [1] * sppCapacity
-        else: self.richness = [0] * sppCapacity
-        self.iInitiate(initRichness)
+        self.area = random.randint(10, aMax)
+        self.coords = [random.uniform(coordRange[0][0], coordRange[0][1]),
+                       random.uniform(coordRange[1][0], coordRange[1][1])]
+        if isML:
+            self.sppCapacity = [1] * sppCapacity
+            self.area = aMax
+            self.coords = [coordRange[0][1]+1, coordRange[1][0]-1]
+        else:
+            self.sppCapacity = [0] * sppCapacity
         self.timeRichness = []
+        self.iInitiate(initRichness)
 
     def iInitiate(self, initRichness):
         """
         Initiates the island with a given species richness sampled from a source
-        :param sourcePop: a list (vector) with ints representing the source population richness
         :param initRichness: the chosen initial richness for the island
         :return: n/a
         """
         if not self.isML:
             for i in range(initRichness):
-                r = random.randint(0,len(self.richness)-1)
-                self.richness[r] = 1
-        self.timeRichness = self.gRichness()
-
-    def update(self):
-        """
-        Updates island with migration and extinction. WIP
-        :return:
-        """
+                r = random.randint(0, len(self.sppCapacity) - 1)
+                self.sppCapacity[r] = 1
         self.timeRichness.append(self.gRichness())
 
     def migrate(self, sourceIsland):
         """
         A function that causes migration of 1 random species from one place to another with a given m probability.
         A species only migrates if it exists in Source Population and is not yet on the island
-        :param mainLand:
-        :param target:
+        !!!The smaller the ALPHA the larger the iRate!!!
+        :param sourceIsland: island from which migrants come
         :return: possibly causes migration of 1 species
         """
         rM = random.uniform(0, 1)
-        #mCoeff = random.uniform(0.8, 1)*(math.e**(-self.distance(sourceIsland)/100000)) #The Bigger, the more likely to migrate
-        epsilon = math.e**(-self.distance(sourceIsland)/100000) #The Bigger, the more likely to migrate
-        iRate = 1-epsilon*(self.gRichness()/len(self.richness))**(self.area/1000) #1000 = maxArea
-        rPosition = random.randint(0, len(self.richness)-1)
-        if self.richness[rPosition] == 0 and \
+        #alpha =  (self.distance(sourceIsland) - self.distance(sourceIsland)) /10000
+        #alpha = math.e**(-self.distance(sourceIsland))*math.e**(self.area)  # TOO LARGE!
+        #alpha = self.area*math.e**(-self.distance(sourceIsland))
+        #alpha = self.distance(sourceIsland)/self.area
+        #alpha = math.e**(-(self.distance(sourceIsland))/self.area)  # 0<=alpha<=1,larger == + likely migration occurs
+        #iRate = 1 - (self.gRichness() / len(self.sppCapacity)) ** alpha  # IR=1-(S/ST)^alpha
+        iRate = 1 - math.e**((-self.distance(sourceIsland)/(self.area/1000)))
+        #iRate = 1 - math.e**((-1/(self.area/1000)))
+        rPosition = random.randint(0, len(self.sppCapacity) - 1)
+        if self.sppCapacity[rPosition] == 0 and \
                 rM < iRate:  # A species migrates with probability m
-            self.richness[rPosition] = 1
+            self.sppCapacity[rPosition] = 1
 
     def extinguish(self):
         """
         A function that causes extinction of 1 random species from one place to another with a given m probability
-        :param mainLand:
-        :param target:
         :return:
         """
         rE = random.uniform(0, 1)
-        #eCoeff = random.uniform(0, 0.1)
-        gamma = random.uniform(0, 0.1)
-        eRate = gamma*(self.gRichness()/len(self.richness))**(1/self.area)
-        rPosition = random.randint(0, len(self.richness)-1)
-        if self.richness[rPosition] == 1 and \
-            rE < eRate:  # A species goes extinct  with probability e
-            self.richness[rPosition] = 0
+        gamma = random.uniform(0, 1)                                 # ??? meaning of this variable?
+        beta = self.area/50                                          # Beta >= 1, =area/minArea
+        eRate = gamma * (self.gRichness() / len(self.sppCapacity)) ** beta    # Er= gamma*(s/ST)^Beta, gamma >=1
+        rPosition = random.randint(0, len(self.sppCapacity) - 1)
+        if self.sppCapacity[rPosition] == 1 and rE < eRate:  # A species goes extinct  with probability e
+            self.sppCapacity[rPosition] = 0
 
     def distance(self, sourceIsland):
         """
         Calculates distance between 2 islands
         :return:
         """
-        dlat = np.deg2rad(self.coords[0] - sourceIsland.coords[0])
-        dlon = np.deg2rad(self.coords[1] - sourceIsland.coords[1])
-        hav = (np.sin(dlat / 2)) ** 2 + np.cos(self.coords[1]) * np.cos(sourceIsland.coords[1])\
-              * (np.sin(dlon / 2)) ** 2
-        return 2 * np.arctan2(sqrt(hav), sqrt(1 - hav)) * 6378100  # c * R , where R=6378100 is the radius of the Earth
-
-    def gArea(self):
-        """
-        :return: area value for each island
-        """
-        return self.area
+        return haversine(self.coords, sourceIsland.coords)
 
     def gRichness(self):
         """
         :return: total species richness for the island
         """
 
-        return sum(1 for x in self.richness if x == 1)
+        return sum(1 for x in self.sppCapacity if x == 1)
 
-    def toString (self):
+    def gTimeRichness(self, minimum, maximum):
+        """
+        Returns the richness from a given island from a set interval of time
+        :param minimum: minimum time
+        :param maximum: maximum time
+        :return: a vector with the time richness
+        """
+        return st.mean(self.timeRichness[minimum:maximum])
+
+    """
+    def isAtEquilivrium(self,island):
+        equi = False
+        if self.gTimeRichness(self.timeRichness[int(len(self.timeRichness)*0.1)],self.gRichness()) 
+        < self.gRichness()-50:
+            equi == True
+        return equi
+    """
+    def toString(self):
         """
         To string method. Shouts a resume of the island atributes
         """
         print("This is a generated island with the following attributes:\n"
               "Area: %d m^2\nCurrent number of species: %d\nCurrent species on island: " %
               (self.area, self.gRichness()))
-        print(self.richness)
+        print(self.sppCapacity)
 
-class chainedArchipelago:
-    """
-    Testing for the archipelago class. A normal archipelago has x islands with different m (for distance) and
-     e (for extintion) parameters. They all assume the same Main Land.
-    """
+##
+# ARCHIPELAGO CLASS
+##
 
-    def __init__(self, sppCapacity, islandNr, initRichness=1, coordRange=[38, -26], aWidth=2):
-        self.islandNr = islandNr
-        self.mainLand = island(sppCapacity, isML=True)
+class Archipelago:
+    def __init__(self, sppCapacity, islandNr, initRichness=1, coordRange=[[39, 35], [-24,-27]], Amax=1000,
+                 iWidth=2, aType="scattered"):
+        """
+        Constructor for the Archipelago class. An archipelago has x islands with different migration and
+       extintion rates. They all assume the same Main Land.
+        """
+        self.islandNr = random.randint(islandNr - iWidth, islandNr + iWidth)
         self.islands = []
+        self.mainLand = Island(sppCapacity, isML=True)
         self.iRichness = []
-        self.timeRichness = [[] for i in range(islandNr)]
+        self.timeRichness = [[] for _ in range(self.islandNr)]
         self.iCoords = []
-        for i in range(islandNr):
-            self.islands.append(island(sppCapacity, False, coordRange, aWidth, initRichness))
+        self.aType = aType
+        for i in range(self.islandNr):
+            self.islands.append(Island(sppCapacity, False, coordRange, Amax, initRichness))
             self.iRichness.append(self.islands[i].gRichness())
             self.timeRichness[i].append(self.iRichness[i])
             self.iCoords.append(self.islands[i].coords)
+        if aType != "scattered": self.sortIslands()
 
-
-    def aUpdate (self, years=1):
+    def aUpdate(self, years=1):
         """
         Updates archipelago with each interaction
         """
         for i in range(years):
+            self.aMigrate()
+            self.aExtinguish()
             for j in range(self.islandNr):
-                self.aMigrate()
-                self.aExtinguish()
                 self.iRichness[j] = self.islands[j].gRichness()
                 self.timeRichness[j].append(self.islands[j].gRichness())
-
 
     def aMigrate(self):
         """
         Causes migration from 1 neighbouring island to a target island with a probability m
-        :param island:
-        :return:
         """
-        for i in range(self.islandNr):
-            for j in range(self.islandNr):
-                self.islands[i].migrate(self.islands[j])
+
+        #Scattered migration
+        if self.aType == "scattered":
+            for i in range(self.islandNr):
+                self.islands[i].migrate(self.mainLand)       # migrate from Main Land
+                for j in range(self.islandNr):
+                    if i != j:
+                        self.islands[i].migrate(self.islands[j])
+            self.iRichness[i] = self.islands[i].gRichness()
+        #Chained migration
+        elif self.aType == "chained":
+            for i in range(self.islandNr):
+                if i == 0:
+                    self.islands[i].migrate(self.mainLand)       # migrate from Main Land
+                else:
+                    self.islands[i].migrate(self.islands[i-1])
+        #Chained with benefits migration
+        elif self.aType == "CWB":
+            for i in range(self.islandNr):
+                self.islands[i].migrate(self.mainLand)       # every island gets spp from Main Land
+                for j in range(i):
+                    self.islands[i].migrate(self.islands[j])
+
 
     def aExtinguish(self):
         """
         Extinguishes one species form an island in an archipelago with a probability e
-        :param island:
-        :return:
         """
         for i in range(self.islandNr):
             self.islands[i].extinguish()
 
-    def gTimeRichness(self, islandNr, min, max):
+    def gTimeRichness(self, minimum, maximum):
         """
         Returns the richness from a given island from a set interval of time
-        :param island:
+        :param minimum: minimum time
+        :param maximum: maximum time
         :return: a vector with the time richness
         """
-        return self.timeRichness[islandNr][min:max]
+        myList = []
+        for i in range(self.islandNr):
+            myList.append(st.mean(self.timeRichness[i][minimum:maximum]))
+        return myList
 
-
-
-    def distMatrix(self, coordsMatrix):
+    def areas(self):
         """
-        Function to give out distance matrix out of 2 collumns of WGS86 coordinates:
-        Latitude first col, longitude 2nd col
-
-        :param coords:
-        :return: A distance matrix (list of lists)
-        """
-        coords = np.deg2rad(coordsMatrix)
-        dist = [[0 for col in range(len(coords))] for row in range(len(coords))]
-        for i in range(len(dist)):
-            for j in range(len(dist[0])):
-                dlat = coords[j][0] - coords[i][0]
-                dlon = coords[j][1] - coords[i][1]
-                hav = (np.sin(dlat / 2)) ** 2 + np.cos(coords[i][1]) * np.cos(coords[j][1]) * (np.sin(dlon / 2)) ** 2
-                c = 2 * np.arctan2(sqrt(hav), sqrt(1 - hav))
-                r = 6378100  # where R is the radius of the Earth
-                dist[i][j] = r * c
-        return dist
-
-    def spatialAutocorr(self, coords):
-        """
-        Integrates spatial autocorrelation. Used in the migration function
-        :return:
-        """
-
-
-    def areas (self):
-        """
-        Method for creating a list with all areas
+        Returns all island areas
         """
         a = []
-        for i in range(islandNr):
-            a.append(islands[i].area)
+        for i in range(self.islandNr):
+            a.append(self.islands[i].area)
         return a
 
+    def speciesArea(self):
+        """
+        :return: a list containing the values of slope, intercept, R^2 and P-value and std error of the species-area
+        relationship
+        """
+        return stats.linregress(self.areas(), self.iRichness)
+
+    def results(self):
+        """
+        :return: a Dataframe in which rows are islands and collumns are island atributes (area, and species)
+        """
+        return pd.DataFrame({"Richness": self.iRichness, "Area": self.areas()})
 
     def toString(self):
         """
         To string method. Shouts a resume of the archipelago's atributes
         """
         print("This is a generated archipelago with the following attributes:\n"
-              "Nr of islands: %d\nRichness is the islands:" %
-              len(self.islands))
+              "Nr of islands: %d" % self.islandNr)
+        print("Area of the islands:")
+        print(self.areas())
+        print("Initial richness in the islands:")
+        [print(self.timeRichness[i][0], end=" ") for i in range(self.islandNr)]
+        print("\nActual richness in the islands:")
         print(self.iRichness)
 
+    def isAtEquilivrium(self, isNr):
+        """
+        Returns true if island is at aquilibrium. TO MOVE INSIDE ISLAND CLASS
+        :param isNr:
+        :return:
+        """
+        equi = True
+        richLim = self.timeRichness[isNr][int(len(self.timeRichness[isNr])*0.9)]
+        if richLim < self.iRichness[isNr]-(self.islandNr*5) or richLim < self.iRichness[isNr]*0.975:
+            equi = False
+        return equi
 
-#Er = extinction rate
-#Ir  immigration rate
+    def sortIslands(self):
+        """
+        Sorts islands per area. Alters the atribute self.islands so that the first ellemnt in the list
+        is the island with the smallest area
+        :return: n/A
+        """
+        a = sorted(self.areas())
+        sortedA = [None]*self.islandNr
+        for i in range(self.islandNr):
+            sortedA[i] = self.islands[self.areas().index(a[i])]
+        self.islands = sortedA
 
-#Ir=1-epsilon(S/ST)^+alpha  ; 0<=alpha<1; E<1
-#Er=gamma(b/ST)^beta ; 1<= beta, gamma<1
+    def distMatrix(self):
+        """
+        Returns a dataframe with the distance matrix
+        :return:
+        """
+        dMatrix = [[]*self.islandNr]*self.islandNr
+        for i in range(self.islandNr):
+            for j in range(self.islandNr):
+              dMatrix.append(self.islands[i].distance(self.islands[j]))
 
-#Ir=Er -> Equilibrium:Seq
-
-#Aplha=A/Amax
-#Beta=1/a ou Beta = Amax/A (need to define Amax)
-
+        return dMatrix
 
--- main.py
+++ main.py
@@ -2,70 +2,112 @@
 Main file for class and object testing. Calls the model with x interactions
 will eventually be the main class of our project
 
-Version 0.1
+Version 0.9
 Author: Diogo Barros
 """
 
 ##
+# TODO: visualization of plots
 # TODO: To read John Harte
 ##
 
-
-from classes import *
+import time
+import os.path
+import math
+import statistics as st
+import numpy as np
+import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
-import statistics
-#from matplotlib import *
-#import numpy as np
+plt.style.use('seaborn-whitegrid')
+from classes import *
 
-#Set variables for the archipelago
-nIslands = 3
+#Set variables for the system: simulating multiple archipelagos:
+archipelagoNr = 3
+arches = []
+islandNr = 10
 sppCapacity = 1000
-initRichness = 100
-years = 2000
-pStart = int(years/2)
-pEnd = years
+initRichness = 10
+years = 1000
+yearStart = int(years/2)
+yearEnd = yearStart+100
 
-#Creating a test chained archipelago:
-a = chainedArchipelago(sppCapacity,nIslands,initRichness)
-a.aUpdate(years)
 
-#Statistics and data visualization:
-print("Initial richness for all islands:")
-for i in range(nIslands):
-    print("Island %d : %d" % (i, a.timeRichness[i][0]))
+#Start simulation and print information for archipelagos
+startTime = time.time()
+for i in range(archipelagoNr):
+    arches.append(Archipelago(sppCapacity, islandNr, initRichness=initRichness, iWidth=0, aType="CWB"))
+    arches[i].aUpdate(years)
+    print("\n###ARCHIPELAGO %d ###" %(i+1))
+    arches[i].toString()
+    print("\nTime elapsed is {} seconds\n".format(time.time()-startTime))
 
-print("\nFinal richness for all islands:")
-for i in range(nIslands):
-    print("Island %d : %d" % (i, a.timeRichness[i][len(a.timeRichness[i])-1]))
+if(islandNr<10):
+    print("Richness in archipelagos at year %d:" % (yearStart))
+    for i in range(archipelagoNr):
+        print("\nArchipelago %d" %(i+1))
+        for j in range(arches[i].islandNr):
+            print("Island %d: %d" % (j+1, arches[i].gTimeRichness(yearStart, yearEnd + 1)[j]))
 
-print("\nIsland mean richness between year %d and %d:" % (pStart,pEnd))
-for i in range(nIslands):
-    print("Island %d mean: %f" % (i, statistics.mean(a.gTimeRichness(i,pStart,pEnd))))
+#Creating my dataframes
+sars=[0]*len(arches)
+scaz=pd.DataFrame([arches[0].speciesArea()])
 
-#Visualizing 1 island
-t=plt.scatter(list(range(-1,years)), a.timeRichness[0])
+for i in range(archipelagoNr):
+    if i > 0:
+        scaz=scaz.append(pd.DataFrame([arches[i].speciesArea()]), ignore_index=True)
+    scaz.rename_axis("Arch nr")
+    sars[i] = pd.DataFrame({
+    "Archipelago":[i+1]*len(arches[i].iRichness),
+    "Richness":arches[i].iRichness,
+    "Area":arches[i].areas()
+    })
+print("\nSCAZ relationships for each archipelago:")
+print(scaz)
+
+data = pd.concat(sars)
+data["Richness"]=data["Richness"].apply(math.log)
+data["Area"]=data["Area"].apply(math.log)
+
+timeRichness= pd.DataFrame({"Time": [i for i in range(years+1)]})
+for z in range(len(arches)):
+    for i in range(arches[z].islandNr):
+        timeRichness["A{} - Island {}".format(i+1, z+1)] = arches[0].timeRichness[i]
+"""
+#Visualizing data: species growth throughout time in an archipelago
+archipelago=0
+rows = 2
+cols = math.ceil(arches[0].islandNr/rows)
+for i in range(arches[0].islandNr):
+    plt.subplot(cols,rows,i+1)
+    plt.scatter(list(range(years+1)), arches[archipelago].timeRichness[i])
+    plt.title("Island %d" %(i+1))
+plt.xlabel("Time")
+plt.ylabel("Richness")
+plt.suptitle("Richness over time in archipelago %d" %(archipelago+1),fontsize=20, weight=20)
+"""
+#Plotting coordinates of islands
+"""
+a = []
+b = []
+for i in range(len(arches[0].iCoords)):
+    a.append(arches[0].iCoords[i][0])
+    b.append(arches[0].iCoords[i][1])
+a.append(arches[0].mainLand.coords[0])
+b.append(arches[0].mainLand.coords[1])
+plt.scatter(a,b)
+plt.title("Island coordinates",fontsize=20, weight=20)
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
 plt.show()
+"""
+
+#Visualizing data: Species-Area relationship per Archipelago
+#sns.lmplot(y="Richness", x="Area", col="Archipelago", data=data).fig.suptitle("Irate=e^(-d+A)")#, size=4,col_wrap=5)
+#plt.subplots_adjust(top=0.85)
+#Outputting data
+#scaz.to_csv("/Users/diogobarros/Desktop/scaz.csv")
+#data.to_csv("/Users/diogobarros/Desktop/sarLogData.csv")
+#timeRichness.to_csv("/Users/diogobarros/Desktop/timeRichness.csv")
 
 
-
-
-
-#Simulating multiple archipelagos:
-
-#Islands characteristics
-#nIslands = 3
-#sppCapacity = 100
-#initRichness = 100
-#years = 100
-
-#Archipelagos characteristics:
-#nrArchipelagos = 2
-#archipelagos = [chainedArchipelago(sppCapacity,nIslands) for i in range(nrArchipelagos)]
-
-#for i in range(nrArchipelagos):
-#    archipelagos[i] = chainedArchipelago(sppCapacity, nIslands, initRichness)
-
-
-
-
-#Hav80952
